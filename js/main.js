100cbfeb2c6a59b135d04f785241114ded4baae5
MinhDuc <ngminhduc1078@gmail.com>
2021-07-24 21:50:06 +0700

    Dark_Light Mode and BackToTop

--- js/main.js
+++ js/main.js
@@ -1,4 +1,22 @@
+// Dark and Light Mode
+function modeChange(){
+    var darkBtn = document.getElementById("dark__btn");
+    var lightBtn = document.getElementById("light__btn");
+    var body = document.body;
+    var isLight = darkBtn.className=="";
+    if (isLight) {
+        lightBtn.classList.remove("d-none");
+        darkBtn.classList.add("d-none");
+        body.classList.add("darkMode");
+    } else {
+        darkBtn.classList.remove("d-none");
+        lightBtn.classList.add("d-none");
+        body.classList.remove("darkMode");
+    }
 
+}
+
+// Navbar Open and close
 function openNav() {
     var navMenu = document.getElementById("navbar_menuContent");
     navMenu.classList.remove("d-none");
@@ -18,3 +36,21 @@ $(document).ready(function(){
         arrows: false
     });
   });
+
+// Back to Top Button
+mybutton = document.getElementById("myBtn");
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0; 
+}
